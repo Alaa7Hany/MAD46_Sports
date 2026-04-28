6d17ef67c3e750edbb34bd0b3521ea205b489f62
Alaa Hany <elgebaly3laa@gmail.com>
2026-04-28 13:19:58 +0300

    Init Structure

---
+++ MAD46_Sports/Model/services/network/AlamofireManager.swift
@@ -0,0 +1,8 @@
+//
+//  AlamofireManager.swift
+//  MAD46_Sports
+//
+//  Created by JETSMobileLabMini3 on 28/04/2026.
+//
+
+import Foundation
---
+++ MAD46_Sports/Model/services/network/NetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkService.swift
+//  MAD46_Sports
+//
+//  Created by JETSMobileLabMini3 on 28/04/2026.
+//
+
+import Foundation
