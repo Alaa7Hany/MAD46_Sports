9e1f39e5b5f1586714adac66817e58a5f5ca61c8
Ahlam <ahlamgomaa304@gmail.com>
2026-04-28 18:39:19 +0300

    impleneted Splash Screen

--- MAD46_Sports/Modules/Splash/Presenter/Splash_Presenter.swift
+++ MAD46_Sports/Modules/Splash/Presenter/Splash_Presenter.swift
@@ -1,8 +1,32 @@
-//
-//  Splash_Presenter.swift
-//  MAD46_Sports
-//
-//  Created by JETSMobileLabMini11 on 28/04/2026.
-//
+
 
 import Foundation
+
+class SplashPresenter {
+    weak var view : SplashView?
+    private let OnboardingKey = "Onboarding"
+    init(view: SplashView) {
+        self.view = view
+    }
+    
+    func start() {
+        let isFirst = !UserDefaults.standard.bool(forKey: OnboardingKey)
+
+               DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+
+                   if isFirst {
+                       UserDefaults.standard.set(true, forKey: self.OnboardingKey)
+                       self.view?.goToOnboarding()
+                   } else {
+                       self.view?.goToHome()
+                   }
+               }
+        
+    }
+    
+    
+}
+protocol SplashView : AnyObject {
+    func goToHome()
+    func goToOnboarding()
+}
--- MAD46_Sports/Modules/Splash/View/SplashViewController.swift
+++ MAD46_Sports/Modules/Splash/View/SplashViewController.swift
@@ -6,24 +6,72 @@
 //
 
 import UIKit
-
-class SplashViewController: UIViewController {
+import Lottie
+class SplashViewController: UIViewController ,SplashView{
+    var presenter : SplashPresenter!
+    private var animationView: LottieAnimationView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        presenter = SplashPresenter(view: self)
+        print("Splash loaded 👀") 
+        setupUI()
+        presenter.start()
+ 
+      
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Splash Appeared")
+    }
+    private func setupUI() {
+           view.backgroundColor = .white
+           setupLottie()
+           setupBallsBackground()
+       }
 
-        // Do any additional setup after loading the view.
+      
+    private func setupLottie() {
+
+        animationView = LottieAnimationView(name: "sports")
+
+        guard let animationView = animationView else { return }
+
+        animationView.frame = view.bounds
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+
+        view.addSubview(animationView)
+    }
+
+    
+       private func setupBallsBackground() {
+           let emitter = CAEmitterLayer()
+           emitter.emitterPosition = CGPoint(x: view.bounds.width / 2, y: view.bounds.height)
+           emitter.emitterShape = .line
+           emitter.emitterSize = CGSize(width: view.bounds.width, height: 1)
+
+           let cell = CAEmitterCell()
+           cell.birthRate = 5
+           cell.lifetime = 10
+           cell.velocity = 60
+           cell.scale = 0.05
+           cell.scaleRange = 0.02
+           cell.contents = UIImage(named: "ball")?.cgImage
+
+           emitter.emitterCells = [cell]
+           view.layer.insertSublayer(emitter, at: 0)
+       }
+    func goToHome() {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func goToOnboarding() {
+     
+        
     }
-    */
+    
+   
 
 }
