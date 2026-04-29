ca57cfc2a9a37b6d481845f853f1bf7c6b9d080d
Ahlam <ahlamgomaa304@gmail.com>
2026-04-29 18:25:42 +0300

    implemented onBoarding Structure

---
+++ MAD46_Sports/Modules/OnBoarding/view/OnboardingCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  UICollectionViewCell.swift
+//  MAD46_Sports
+//
+//  Created by JETSMobileLabMini11 on 29/04/2026.
+//
+
+import UIKit
+
+class OnboardingCollectionViewCell: UICollectionViewCell {
+    
+    
+    func setup(_ slide: OnBoardingModel) {
+   
+    }
+}
