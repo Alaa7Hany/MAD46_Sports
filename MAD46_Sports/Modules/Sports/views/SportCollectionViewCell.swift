281e899eb89456188aee9c23934a049a1d07d1e5
Alaa Hany <elgebaly3laa@gmail.com>
2026-05-05 14:17:02 +0300

    refactored sports layout, implemented banner view, implemented theme and sound buttons

--- MAD46_Sports/Modules/Sports/views/SportCollectionViewCell.swift
+++ MAD46_Sports/Modules/Sports/views/SportCollectionViewCell.swift
@@ -11,17 +11,58 @@ class SportCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var lblName: UILabel!
     @IBOutlet weak var imageV: UIImageView!
+    
+    private let gradientLayer = CAGradientLayer()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        self.layer.cornerRadius = 30
-        self.layer.masksToBounds = true
-        imageV.clipsToBounds = true
+        setupCardStyling()
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = self.bounds
     }
     
     func configureCell(name: String, image: String){
         lblName.text = name
         imageV.image = UIImage(named: image)
+    }
+    
+    private func setupCardStyling() {
+        self.layer.cornerRadius = 20
+        self.layer.masksToBounds = false
+        
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.15
+        self.layer.shadowOffset = CGSize(width: 0, height: 4)
+        self.layer.shadowRadius = 10
+        
+        imageV.layer.cornerRadius = 20
+        imageV.clipsToBounds = true
+        
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.black.withAlphaComponent(0.7).cgColor
+        ]
+        gradientLayer.locations = [0.6, 1.0]
+        imageV.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            toggleAnimation(isHighlighted)
+        }
+    }
+
+    private func toggleAnimation(_ show: Bool) {
+        let transform = show ? CGAffineTransform(scaleX: 0.95, y: 0.95) : .identity
+        let bgColor = show ? UIColor.systemGray6.withAlphaComponent(0.5) : .clear
+
+        UIView.animate(withDuration: 0.2, delay: 0, options: [.beginFromCurrentState, .allowUserInteraction]) {
+            self.transform = transform
+            self.contentView.backgroundColor = bgColor
+        }
     }
 
 }
--- MAD46_Sports/Modules/Sports/views/SportsViewController.swift
+++ MAD46_Sports/Modules/Sports/views/SportsViewController.swift
@@ -1,107 +1,233 @@
 import UIKit
 
 protocol SportsViewProtocol: AnyObject {
-    func displaySports()}
+    func displaySports()
+}
 
-
+// MARK: - Main View Controller
 class SportsViewController: UIViewController {
 
-    @IBOutlet weak var switchtheme: UISwitch!
+    // MARK: Outlets
     @IBOutlet weak var collectionView: UICollectionView!
-
-    var presenter: SportsPresenterProtocol!
+    @IBOutlet weak var bannerCollectionview: UICollectionView!
+    @IBOutlet weak var btnSound: UIButton!
+    @IBOutlet weak var btnTheme: UIButton!
     
+    // MARK: Properties
+    var presenter: SportsPresenterProtocol!
+    private var bannerTimer: Timer?
+    private var currentBannerIndex = 0
+    private let infiniteMultiplier = 1000
 
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
- 
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
-        let nib = UINib(nibName: "SportCollectionViewCell", bundle: nil)
-        collectionView.register(nib, forCellWithReuseIdentifier: "sportsCell")
-        
-        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-                flowLayout.estimatedItemSize = .zero
-            }
-        
+        setupInitialUI()
+        setupCollectionViews()
         presenter.viewDidLoad()
-        
-        let isDarkMode = UserDefaults.standard.bool(forKey: Constants.Defaults.themeKey)
-        switchtheme.isOn = isDarkMode
+        startBannerTimer()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        bannerTimer?.invalidate()
     }
 }
 
-extension SportsViewController: SportsViewProtocol {
+// MARK: - UI Setup
+private extension SportsViewController {
     
-    func displaySports() {
-        DispatchQueue.main.async {
-             self.collectionView.reloadData()
+    func setupInitialUI() {
+        let isDarkMode = UserDefaults.standard.bool(forKey: Constants.Defaults.themeKey)
+        let themeIcon = isDarkMode ? "moon.fill" : "sun.max.fill"
+        btnTheme.setImage(UIImage(systemName: themeIcon), for: .normal)
+        btnSound.setImage(UIImage(systemName: "mic.fill"), for: .normal)
+    }
+    
+    func setupCollectionViews() {
+        let nib = UINib(nibName: Constants.Cells.sportCollectionCell, bundle: nil)
+        
+        // --- Main Grid Setup ---
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(nib, forCellWithReuseIdentifier: "sportsCell")
+        
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = .zero
+        }
+        
+        // --- Banner Setup ---
+        bannerCollectionview.delegate = self
+        bannerCollectionview.dataSource = self
+        bannerCollectionview.register(nib, forCellWithReuseIdentifier: "sportsCell")
+        bannerCollectionview.isPagingEnabled = true
+        bannerCollectionview.showsHorizontalScrollIndicator = false
+        
+        if let bannerLayout = bannerCollectionview.collectionViewLayout as? UICollectionViewFlowLayout {
+            bannerLayout.scrollDirection = .horizontal
+            bannerLayout.minimumLineSpacing = 0
+            bannerLayout.estimatedItemSize = .zero
         }
     }
 }
 
+// MARK: - Actions
+extension SportsViewController {
+    
+    @IBAction func onThemechanged(_ sender: Any) {
+        let isCurrentlyDark = UserDefaults.standard.bool(forKey: Constants.Defaults.themeKey)
+        let newDarkModeState = !isCurrentlyDark
+        
+        UserDefaults.standard.set(newDarkModeState, forKey: Constants.Defaults.themeKey)
+        
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first {
+            window.overrideUserInterfaceStyle = newDarkModeState ? .dark : .light
+        }
+        
+        let iconName = newDarkModeState ? "moon.fill" : "sun.max.fill"
+        btnTheme.setImage(UIImage(systemName: iconName), for: .normal)
+    }
+    
+    @IBAction func onSoundChanged(_ sender: Any) {
+        let currentImage = btnSound.image(for: .normal)
+        let newIcon = currentImage == UIImage(systemName: "mic.fill") ? "mic.slash.fill" : "mic.fill"
+        btnSound.setImage(UIImage(systemName: newIcon), for: .normal)
+    }
+}
+
+// MARK: - Banner Timer Logic
+private extension SportsViewController {
+    
+    func startBannerTimer() {
+        bannerTimer?.invalidate()
+        
+        bannerTimer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true) { [weak self] _ in
+            self?.scrollToNextBanner()
+        }
+        
+        if let timer = bannerTimer {
+            RunLoop.main.add(timer, forMode: .common)
+        }
+    }
+
+    func scrollToNextBanner() {
+        let count = presenter.getSportsCount()
+        guard count > 0 else { return }
+        
+        let totalItems = count * infiniteMultiplier
+        
+        if currentBannerIndex >= totalItems - count {
+            let middleIndex = (count * infiniteMultiplier) / 2
+            currentBannerIndex = middleIndex - (middleIndex % count)
+            
+            bannerCollectionview.scrollToItem(at: IndexPath(item: currentBannerIndex, section: 0),
+                                              at: .centeredHorizontally,
+                                              animated: false)
+        }
+        
+        currentBannerIndex += 1
+        
+        let indexPath = IndexPath(item: currentBannerIndex, section: 0)
+        bannerCollectionview.scrollToItem(at: indexPath,
+                                          at: .centeredHorizontally,
+                                          animated: true)
+    }
+}
+
+// MARK: - SportsViewProtocol
+extension SportsViewController: SportsViewProtocol {
+    func displaySports() {
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+            self.bannerCollectionview.reloadData()
+            
+            let count = self.presenter.getSportsCount()
+            if count > 0 {
+                let middleIndex = (count * self.infiniteMultiplier) / 2
+                self.currentBannerIndex = middleIndex - (middleIndex % count)
+                self.bannerCollectionview.scrollToItem(at: IndexPath(item: self.currentBannerIndex, section: 0), at: .centeredHorizontally, animated: false)
+            }
+        }
+    }
+}
+
+// MARK: - UICollectionView DataSource & Delegate
 extension SportsViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == bannerCollectionview {
+            return presenter.getSportsCount() * infiniteMultiplier
+        }
         return presenter.getSportsCount()
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sportsCell", for: indexPath) as! SportCollectionViewCell
         
-         let sport = presenter.getSport(at: indexPath.row)
-         cell.lblName.text = sport.sportName
-         cell.imageV.image = UIImage(named: sport.sportThumb ?? "")
+        let actualIndex = collectionView == bannerCollectionview ? (indexPath.row % presenter.getSportsCount()) : indexPath.row
+        let sport = presenter.getSport(at: actualIndex)
         
+        cell.lblName.text = sport.sportName
+        cell.imageV.image = UIImage(named: sport.sportThumb ?? "")
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("navigate ")
-        presenter.didSelectSport(at: indexPath.row)
-        print("navigated ")
-
+        let actualIndex = collectionView == bannerCollectionview ? (indexPath.row % presenter.getSportsCount()) : indexPath.row
+        presenter.didSelectSport(at: actualIndex)
     }
 }
 
-
+// MARK: - UICollectionView Delegate FlowLayout
 extension SportsViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let padding: CGFloat = 10
-        let totalPadding = padding * 3
-        let availableWidth = collectionView.bounds.width - totalPadding
-        let cellWidth = availableWidth / 2
-        
-        return CGSize(width: cellWidth, height: cellWidth)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
+        if collectionView == bannerCollectionview {
+            return CGSize(width: collectionView.bounds.width, height: collectionView.bounds.height)
+        } else {
+            let padding: CGFloat = 10
+            let availableWidth = collectionView.bounds.width - (padding * 3)
+            let cellWidth = availableWidth / 2
+            return CGSize(width: cellWidth, height: cellWidth)
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
+        return collectionView == bannerCollectionview ? 0 : 10
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        return collectionView == bannerCollectionview ? .zero : UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+}
+
+// MARK: - UIScrollViewDelegate (Manual Scroll Tracking)
+extension SportsViewController {
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        if scrollView == bannerCollectionview {
+            bannerTimer?.invalidate()
+        }
     }
     
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        if scrollView == bannerCollectionview {
+            let pageWidth = scrollView.bounds.width
+            guard pageWidth > 0 else { return }
+            
+            currentBannerIndex = Int(round(scrollView.contentOffset.x / pageWidth))
+            startBannerTimer()
+        }
+    }
     
-    
-    @IBAction func themeToggled(_ sender: UISwitch) {
-        let isDarkMode = sender.isOn
-        
-        UserDefaults.standard.set(isDarkMode, forKey: Constants.Defaults.themeKey)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
-            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-               let window = windowScene.windows.first {
-                
-                window.overrideUserInterfaceStyle = isDarkMode ? .dark : .light
-            }
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if scrollView == bannerCollectionview && !decelerate {
+            let pageWidth = scrollView.bounds.width
+            guard pageWidth > 0 else { return }
+            
+            currentBannerIndex = Int(round(scrollView.contentOffset.x / pageWidth))
+            startBannerTimer()
         }
     }
 }
